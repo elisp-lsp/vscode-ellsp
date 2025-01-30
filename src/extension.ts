8ce6245094794f228a61f24da833ed79dd686227
JenChieh <jcs090218@gmail.com>
2025-01-29 20:11:26 -0800

    feat: fix executable name

--- src/extension.ts
+++ src/extension.ts
@@ -12,20 +12,10 @@ import {
 
 let client: LanguageClient;
 
-// Return Ellsp executable name!
-function getExec() {
-	switch (process.platform) {
-		case 'darwin': return "ellsp-macos";
-		case 'win32': return "ellsp-win";
-		case 'linux': return "ellsp-linux";
-	}
-	return 'ellsp';
-}
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const serverExecutable = { command: 'eask', args: ['exec', getExec()] };
+	const serverExecutable = { command: 'eask', args: ['exec', "ellsp"] };
 
 	const serverOptions: ServerOptions = {
 		run: serverExecutable,
